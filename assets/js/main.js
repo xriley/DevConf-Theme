6246f2ee2e7965a96c242d8fab7f436e554f9762
Sarvesh Patkar <spatkar131201@gmail.com>
2022-12-04 14:29:17 +0530

    date changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Oct 12, 2028").getTime();
+var target_date = new Date("Dec 18, 2022 09:30").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
